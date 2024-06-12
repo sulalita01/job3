6ed5781aea0625e1c5dc86507dc170f30ad61c7a
sulalita kunin <sulalitakunin@gmail.com>
2024-06-12 14:09:21 +0700

    three commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private page: NavController) {}
+  goabout(){
+    this.page.navigateForward ('/tabs/tab2');
+  }
+
+  gocontact(){
+    this.page.navigateForward ('/tabs/tab3');
+  }
 
 }
